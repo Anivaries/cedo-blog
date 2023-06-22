8535af27a957bd49aad2109d837bd6f65e887c60
Anivaries <k0m4r4c00@gmail.com>
2023-06-22 20:02:12 +0200

    views/models added

--- base/models.py
+++ base/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Blog(models.Model):
     text = models.TextField()
     # author
-    title = models.CharField(max_length=50)
-    # date
+    title = models.CharField(max_length=120)
+    pub_date = models.DateTimeField("date published", auto_created = True)
+    slug = models.SlugField(default="slug-one", max_length=70)
     # tags
+
+    def __str__(self) -> str:
+        return self.title
--- base/views.py
+++ base/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.views import generic
+from django.views.generic import DetailView
+from .models import Blog
 
-# Create your views here.
+class BlogView(DetailView):
+    model = Blog
+    template_name = "blog_content.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["blog"] = Blog.objects.filter(pk=kwargs['pk'])
+        return context
+    
