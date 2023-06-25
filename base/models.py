a090ca64fdfcc289829b51b938d856e9295170bf
Anivaries <k0m4r4c00@gmail.com>
2023-06-25 19:06:30 +0200

    css updates

--- base/models.py
+++ base/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Blog(models.Model):
+    game_name = models.CharField(max_length=30)
     text = models.TextField()
-    # author
     title = models.CharField(max_length=120)
     pub_date = models.DateTimeField("date published", auto_created = True)
     slug = models.SlugField(default="slug-one", max_length=70)
+    # author
     # tags
 
     def __str__(self) -> str:
